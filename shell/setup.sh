bd5a2ff1a26ef98afd8ca0e8c8fe4e8bd46cac12
Charlton Cheng <charltoncheng86@gmail.com>
2019-04-07 17:18:40 +1000

    SHELL - Setup: Tidy up usage comment

--- shell/setup.sh
+++ shell/setup.sh
@@ -3,13 +3,14 @@
 ######################
 # Requirements:
 # - SSH must be setup between Remote Host & Github for clone/pull
+# - SSH Agent needs to be eval (required for AWS EC2)
 # - Executable permission is added for this Shell Script file via `chmod +x <fileName>.sh` (under project folder)
 
 # Usage Syntax:
 # 1. Local - Copy the SSH keys from Local to Remote ".ssh" folder, Login
 #   scp -i ~/.ssh/<awsKey>.pem <sshKeyPath>/{id_rsa,id_rsa.pub} <userName>@<vpsPublicDnsIpAddr>:/home/<userName>/.ssh/
 #   ssh -i ~/.ssh/<awsKey>.pem <userName>@<vpsPublicDnsIpAddr>
-
+#
 # 2. Remote - Setup project & Run its Setup Shell Script:
 #   eval $(ssh-agent) && cd ~/ && mkdir <projectRootName> && cd <projectRootName> && git clone <gitProjectUrl>
 #   cd <projectFolder>/<shellFolder> && chmod +x <setupScript>.sh && source ./<setupScript>.sh
@@ -18,7 +19,7 @@
 # 1. Local:
 # scp -i ~/.ssh/aws-ec2.pem ssh/{id_rsa,id_rsa.pub} ubuntu@ec2-13-210-68-168.ap-southeast-2.compute.amazonaws.com:/home/ubuntu/.ssh/
 # ssh -i ~/.ssh/aws-ec2.pem ubuntu@ec2-13-210-68-168.ap-southeast-2.compute.amazonaws.com
-
+#
 # 2. Remote:
 # eval $(ssh-agent) && cd ~/ && mkdir dev && cd dev && git clone git@github.com:CharltonC/deploy-ec2.git
 # cd deploy-ec2/shell && chmod +x setup.sh && source ./setup.sh
