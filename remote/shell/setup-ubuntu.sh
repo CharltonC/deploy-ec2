79e5380d6d69f5fb6aa9d93d5c7c026204cd105c
Charlton Cheng <charltoncheng86@gmail.com>
2019-04-12 09:02:54 +1000

    Added test script

--- remote/shell/setup-ubuntu.sh
+++ remote/shell/setup-ubuntu.sh
@@ -12,6 +12,6 @@ sudo apt-get upgrade -y
 sudo apt-get install -y $UBUNTU_APT_DEP_LIST
 
 # Install Python Virtual Env. as Global (after python installed from the ubuntu packages)
-pip3 install virtualenv
+sudo -h pip3 install virtualenv
 
 logEnd 'Ubuntu'
