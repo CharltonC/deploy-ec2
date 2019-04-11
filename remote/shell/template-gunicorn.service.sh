059271b04d06541b1211e026ba0ed19cf24b9572
Charlton Cheng <charltoncheng86@gmail.com>
2019-04-11 13:32:22 +1000

    SHELL - Setup: Fixed Path issue by changing all to Absolute path

--- remote/shell/template-gunicorn.service.sh
+++ remote/shell/template-gunicorn.service.sh
@@ -6,8 +6,8 @@ After=network.target
 [Service]
 User=$UBUNTU_USERNAME
 Group=www-data
-WorkingDirectory=$PROJECT_FOLDER_ABS_PATH/$PROJECT_DJANGO_FOLDER
-ExecStart=$PROJECT_FOLDER_ABS_PATH/venv/bin/gunicorn --access-logfile - --workers 3 --bind unix:$PROJECT_FOLDER_ABS_PATH/$PROJECT_DJANGO_FOLDER.sock $PROJECT_DJANGO_FOLDER.wsgi:application
+WorkingDirectory=$PROJECT_FOLDER_PATH/$PROJECT_DJANGO_FOLDER
+ExecStart=$PROJECT_FOLDER_PATH/venv/bin/gunicorn --access-logfile - --workers 3 --bind unix:$PROJECT_FOLDER_PATH/$PROJECT_DJANGO_FOLDER.sock $PROJECT_DJANGO_FOLDER.wsgi:application
 
 [Install]
 WantedBy=multi-user.target
