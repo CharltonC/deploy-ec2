1c196317b2d74137fe4ec22bcce0b15496a09a8e
Charlton Cheng <charltoncheng86@gmail.com>
2019-04-15 11:18:10 +1000

    DJANGO - Url: Added a dummy response for default Url

--- deploy_ec2/deploy_ec2/urls.py
+++ deploy_ec2/deploy_ec2/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def dummyResponseFn(request):
+    return HttpResponse('automatic deployment test successful')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', dummyResponseFn)
 ]
